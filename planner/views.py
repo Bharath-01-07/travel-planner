b7654b5708578449e0773e3df6ef6ba4fb34b5aa
Your Name <your_email@example.com>
2026-06-03 09:35:05 +0530

    Restore frontend

--- planner/views.py
+++ planner/views.py
@@ -1,80 +1,14 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from .models import Trip, Expense
-from .forms import TripForm, DestinationForm, ExpenseForm, ScheduleForm
-from .weather import get_weather
-
-
 def home(request):
-    return HttpResponse("Travel Planner Working Successfully!")
+    trips = Trip.objects.all()
+    total_expense = sum(
+        expense.amount for expense in Expense.objects.all()
+    )
 
-
-@login_required
-def add_trip(request):
-    if request.method == 'POST':
-        form = TripForm(request.POST)
-
-        if form.is_valid():
-            trip = form.save(commit=False)
-            trip.user = request.user
-            trip.save()
-            return redirect('/')
-    else:
-        form = TripForm()
-
-    return render(request, 'add_trip.html', {'form': form})
-
-
-@login_required
-def add_destination(request):
-    if request.method == 'POST':
-        form = DestinationForm(request.POST)
-        if form.is_valid():
-            destination = form.save(commit=False)
-            destination.save()
-            return redirect('/')
-    else:
-        form = DestinationForm()
-
-    return render(request, 'add_destination.html', {'form': form})
-
-
-@login_required
-def add_expense(request):
-    if request.method == 'POST':
-        form = ExpenseForm(request.POST)
-
-        if form.is_valid():
-            expense = form.save(commit=False)
-            expense.save()
-            return redirect('/')
-    else:
-        form = ExpenseForm()
-
-    return render(request, 'add_expense.html', {'form': form})
-
-
-@login_required
-def add_schedule(request):
-    if request.method == 'POST':
-        form = ScheduleForm(request.POST)
-
-        if form.is_valid():
-            schedule = form.save(commit=False)
-            schedule.save()
-            return redirect('/')
-    else:
-        form = ScheduleForm()
-
-    return render(request, 'add_schedule.html', {'form': form})
-
-
-def weather_view(request):
-    weather_data = None
-
-    if request.method == 'POST':
-        city = request.POST.get('city')
-        weather_data = get_weather(city)
-
-    return render(request, 'weather.html', {'weather': weather_data})
+    return render(
+        request,
+        'home.html',
+        {
+            'trips': trips,
+            'total_expense': total_expense
+        }
+    )
